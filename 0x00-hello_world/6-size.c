4b05f7b6977abac0cf7b7a8520f9c459c6cf019a
Halabitimoty <halabitimoty@yahoo.com>
2022-09-08 10:02:09 -0700

    0x00. tasks

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * *main - Entry point
+ * *Return: Always 0 (Success)
+ * *Command: it's prints out the size of char,
+ * *int, long int, long long int and float
+ * */
+
+int main(void)
+{
+	prinf("Size of a char: %lu bytes(s)",sizeof(char)");
+	prinf("Size of an int: %lu bytes(s)",sizeof(int)");
+	prinf("Size of a long int: %lu bytes(s)",(long)sizeof(int)");
+	prinf("Size of a long long: %lu bytes(s)",(long long)sizeof(int)");
+	prinf("Size of a float: %lu bytes(s)",sizeof(float)");
+	return (0);
+}
