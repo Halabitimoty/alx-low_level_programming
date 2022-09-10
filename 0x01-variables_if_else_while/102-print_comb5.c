6bf7d0cb5a4a506921ed69301e44554f38a08433
Halabitimoty <halabitimoty@yahoo.com>
2022-09-09 19:21:57 -0700

    Advanced task 2

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -27,9 +27,10 @@ int main(void)
 						putchar(j);
 						if (c + i + k + j == 227 && c == 57)
 						{
-							break;
+						break;
 						}
-						else{
+						else
+						{
 							putchar(',');
 							putchar(' ');
 						}
