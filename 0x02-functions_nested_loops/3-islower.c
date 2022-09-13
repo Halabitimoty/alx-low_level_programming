8a2b2d2707dfcf80cd557aa08e04d83c8fc5a91a
Halabitimoty <halabitimoty@yahoo.com>
2022-09-13 11:16:36 -0700

    task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * islower function - checks if an input
+ * character is lower or not
+ *
+ * Return - Returns 1 if true and 0 if false
+ *
+ */
+
+int _islower(int c)
+{
+	char i;
+
+	if (islower(i))
+		return (1);
+
+	else
+		return (0)
+}
