26ba9fed549fa492d937b5a3e39f9c9d236bc152
Halabitimoty <halabitimoty@yahoo.com>
2022-09-13 08:28:11 -0700

    main.h included

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * File : main.h
+ * Auth : ALABI TIMOTHY
+ * Desc : Header file containing Function Prototypes
+ * for all Functions.
+ */
+
+int _putchar(char c);
+
+#endif
