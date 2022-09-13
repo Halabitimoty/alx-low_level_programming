294b51279dd5012d3d34f80a68890ee81c2d23e4
Halabitimoty <halabitimoty@yahoo.com>
2022-09-13 09:04:18 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ * Task - Function that prints alphabeth 
+ * in lowercase
+ * Auth - ALABI TIMOTHY
+ */
+
+#include "main.h"
+
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
