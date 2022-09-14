222e1b9c8da4ff4a764ce0d20f4c9b6839a624b4
Halabitimoty <halabitimoty@yahoo.com>
2022-09-13 17:18:56 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - Adds two integers
+ * @num1: The first integer
+ * @num2: The second integer
+ * Return: The result of the addition
+ * Auth - ALABI TIMOTHY
+ */
+
+int add(int numb1, int numb2)
+{
+	int result = numb1 + numb2;
+
+	return (result);
+}
