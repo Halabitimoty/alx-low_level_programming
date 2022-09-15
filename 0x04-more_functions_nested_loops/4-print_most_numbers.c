88df024fc0aedfac34b00476db9a12e5512de23b
Halabitimoty <halabitimoty@yahoo.com>
2022-09-15 12:58:45 -0700

    task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -13,9 +13,7 @@ void print_most_numbers(void)
 
 	for (i = 0; i <= 9)
 	{
-		if ((i == 2) || (i ==4))
-			continue;
-		else
+		if ((i != 2) || (i != 4))
 			_putchar(i + '0');
 	}
 		_putchar('\n');
