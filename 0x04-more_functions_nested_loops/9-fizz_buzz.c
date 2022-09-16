94c95f8e434c4289164c7d5f69db2d7d81188c1b
Halabitimoty <halabitimoty@yahoo.com>
2022-09-15 18:59:38 -0700

    task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,15 +14,12 @@ int main(void)
 	{
 		if ((i % 15) == 0)
 			printf("FizzBuzz");
-		
 		else if ((i % 5) == 0)
 			printf("Buzz");
-		
 		else if ((i % 3) == 0)
 			printf("Fizz");
 		else
 			printf("%d", i);
-		
 		printf(' ');
 	}
 	printf("\n");
