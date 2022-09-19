cc7aa3fab5f33d4ebf99233f05e35a7b56f8b9a3
Halabitimoty <57334680+Halabitimoty@users.noreply.github.com>
2022-09-19 20:54:29 +0100

    task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_array - function that prints n elements of an array of integers,
+ * followed by a new line.
+ *
+ *  @a: int pointer
+ *  @n: int pointer (size of Array)
+ */
+
+void print_array(int *a, int n)
+{
+	for (i = 0; i < n; i++)
+	{
+		_putchar(a[1]);
+	}
+	_putchar('\n');
+}
