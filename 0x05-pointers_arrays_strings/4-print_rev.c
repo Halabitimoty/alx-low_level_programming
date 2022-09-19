a76a5393b9f1281f4bda5d3ba8bd34037491fe98
Halabitimoty <57334680+Halabitimoty@users.noreply.github.com>
2022-09-19 19:51:15 +0100

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_rev -  function that prints a string, in reverse,
+ * followed by a new line
+ *
+ *  @s: char pointer
+ */
+
+void print_rev(char *s)
+{
+	int count;
+	int str_len;
+
+	count = 0;
+
+	while (*s != '\0')
+	{
+		s++;
+		count++;
+	}
+
+	for (str_len = count - 1; str_len >= 0; str_len--)
+	{
+		_putchar(s[str_len]);
+	}
+}
