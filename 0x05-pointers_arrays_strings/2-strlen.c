bf5324b2325adf1d27b6317b0ef842c3dde232e7
Halabitimoty <57334680+Halabitimoty@users.noreply.github.com>
2022-09-19 18:47:13 +0100

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strlen - function that returns the length of a string
+ *
+ * @s: pointer parameter
+ */
+
+int _strlen(char *s)
+{
+	int count;
+
+	count = 0;
+
+	while (*s != '\0')
+	{
+		count++;
+	}
+
+	return (count);
+}
