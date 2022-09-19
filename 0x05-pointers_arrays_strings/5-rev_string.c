2cb1efefc86cf6df5f6a7563e3d501cf3a91d274
Halabitimoty <57334680+Halabitimoty@users.noreply.github.com>
2022-09-19 20:21:24 +0100

    task 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -15,14 +15,14 @@ void rev_string(char *s)
 	count = 0;
 
 	while (*(s + count) != '\0')
-		count++; // E.g count is 10
+		count++;
 
-	str_len = count; // str_len is 10
+	str_len = count;
 
 	for (count = str_len - 1; count >= (str_len / 2); count--)
 	{
-		temp = s[count];				   // last char
-		s[count] = s[str_len - count - 1]; // 10 - 10 - 1
+		temp = s[count];
+		s[count] = s[str_len - count - 1];
 		s[str_len - count - 1] = temp;
 	}
 }
