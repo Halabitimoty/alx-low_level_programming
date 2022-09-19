cf8835c2058e78c779cd2a93019af6ee372573d0
Halabitimoty <57334680+Halabitimoty@users.noreply.github.com>
2022-09-19 21:09:02 +0100

    task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_array - function that prints n elements of an array of integers,
+ * followed by a new line.
+ *
+ *  @dest: char pointer destination
+ *  @src: char pointer (size of Array)
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+	int str_len;
+
+	i = 0;
+	str_len = 0;
+
+	while (*(str + str_len) != '\0')
+		str_len++;
+
+	while (i < str_len)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+
+	return (des);
+}
