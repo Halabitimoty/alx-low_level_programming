6ae55d6dd4ab87b4759ffc4430c49acd7818fa00
Halabitimoty <57334680+Halabitimoty@users.noreply.github.com>
2022-09-22 21:00:40 +0100

    Advanced Task 103

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -15,15 +15,11 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 
 	for (i = 0; n1[i]; i++)
 		;
-
 	for (j = 0; n2[j]; j++)
 		;
-
 	if (i > size_r || j > size_r)
 		return (0);
-
 	m = 0;
-
 	for (i -= 1, j -= 1, k = 0; k < size_r - 1; i--, j--, k++)
 	{
 		n = m;
@@ -38,18 +34,14 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		m = n / 10;
 		r[k] = n % 10 + '0';
 	}
-
 	r[k] = '\0';
-
 	if (i >= 0 || j >= 0 || m)
 		return (0);
-
 	for (k -= 1, l = 0; l < k; k--, l++)
 	{
 		m = r[k];
 		r[k] = r[l];
 		r[l] = m;
 	}
-
 	return (r);
 }
