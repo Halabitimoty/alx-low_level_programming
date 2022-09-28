e19803bc448d05de826b652cecdf326867cfbb57
Halabitimothy <Halabitimoty@yahoo.com>
2022-09-28 10:59:14 +0100

    Task 1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,7 +1,17 @@
+#include "main.h"
 /**
- * main - check the code
+ * _print_rev_recursion - function that prints a string in reverse.
  *
- * Return: Always 0.
+ * Return: Nothing.
  *
  * Author - ALABI TIMOTHY
  */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
