68173b3068a01976ebf540e060d5b550937ead45
Halabitimoty <halabitimoty@yahoo.com>
2022-09-28 20:34:05 -0700

    Advanced

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -15,18 +15,16 @@ int wildcmp(char *s1, char *s2);
  */
 int strlen_no_wilds(char *str)
 {
-		int len = 0, index = 0;
+	int len = 0, index = 0;
 
 	if (*(str + index))
 	{
 		if (*str != '*')
-												len++;
-
-										index++;
-												len += strlen_no_wilds(str + index);
-													}
-
-				return (len);
+			len++;
+		index++;
+		len += strlen_no_wilds(str + index);
+	}
+	return (len);
 }
 
 /**
@@ -35,41 +33,43 @@ int strlen_no_wilds(char *str)
  *    * @wildstr: The string to be iterated through.
  */
 
+
 void iterate_wild(char **wildstr)
 {
 	if (**wildstr == '*')
 	{
 		(*wildstr)++;
-									iterate_wild(wildstr);
-										}
+		iterate_wild(wildstr);
+	}
 }
+
 
 /**
  *  * postfix_match - Checks if a string str matches the postfix of
  *   *                 another string potentially containing wildcards.
  *    * @str: The string to be matched.
  *     * @postfix: The postfix.
- *      *
- *      Return: If str and postfix are 
+ *     Return :If str and postfix are
  *       identical - a pointer to the null byte
  *        * located at the end of postfix.
- *         *         Otherwise - a pointer to the first unmatched character in postfix.
+ *        *Otherwise - a pointer to the first
+ *         unmatched character in postfix.
 */
+
 char *postfix_match(char *str, char *postfix)
 {
-		int str_len = strlen_no_wilds(str) - 1;
-			int postfix_len = strlen_no_wilds(postfix) - 1;
+	int str_len = strlen_no_wilds(str) - 1;
+	int postfix_len = strlen_no_wilds(postfix) - 1;
 
 if (*postfix == '*')
 iterate_wild(&postfix);
 
-					if (*(str + str_len - postfix_len) == *postfix && *postfix != '\0')
-							{
-										postfix++;
-												return (postfix_match(str, postfix));
-													}
-
-						return (postfix);
+	if (*(str + str_len - postfix_len) == *postfix && *postfix != '\0')
+	{
+		postfix++;
+		return (postfix_match(str, postfix));
+								}
+	return (postfix);
 }
 
 /**
@@ -85,15 +85,13 @@ int wildcmp(char *s1, char *s2)
 {
 if (*s2 == '*')
 {
-							iterate_wild(&s2);
-									s2 = postfix_match(s1, s2);
-										}
+	iterate_wild(&s2);
+	s2 = postfix_match(s1, s2);
+}
 
 	if (*s2 == '\0')
 		return (1);
-
-				if (*s1 != *s2)
-							return (0);
-
-					return (wildcmp(++s1, ++s2));
+if (*s1 != *s2)
+	return (0);
+return (wildcmp(++s1, ++s2));
 }
