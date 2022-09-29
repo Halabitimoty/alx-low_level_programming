d2061750b5272caf64ba8c680d78be91d70a8eec
Halabitimoty <halabitimoty@yahoo.com>
2022-09-28 19:38:30 -0700

    Advanced

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -50,9 +50,9 @@ void iterate_wild(char **wildstr)
  *    * @str: The string to be matched.
  *     * @postfix: The postfix.
  *      *
- *       * Return: If str and postfix are 
+ *      Return: If str and postfix are 
  *       identical - a pointer to the null byte
- *        *                                            located at the end of postfix.
+ *        * located at the end of postfix.
  *         *         Otherwise - a pointer to the first unmatched character in postfix.
 */
 char *postfix_match(char *str, char *postfix)
@@ -60,8 +60,8 @@ char *postfix_match(char *str, char *postfix)
 		int str_len = strlen_no_wilds(str) - 1;
 			int postfix_len = strlen_no_wilds(postfix) - 1;
 
-				if (*postfix == '*')
-							iterate_wild(&postfix);
+if (*postfix == '*')
+iterate_wild(&postfix);
 
 					if (*(str + str_len - postfix_len) == *postfix && *postfix != '\0')
 							{
@@ -79,17 +79,18 @@ char *postfix_match(char *str, char *postfix)
  *     *
  *      * Return: If the strings can be considered identical - 1.
  *       *         Otherwise - 0.
- *        */
+ */
+
 int wildcmp(char *s1, char *s2)
 {
-		if (*s2 == '*')
-				{
+if (*s2 == '*')
+{
 							iterate_wild(&s2);
 									s2 = postfix_match(s1, s2);
 										}
 
-			if (*s2 == '\0')
-						return (1);
+	if (*s2 == '\0')
+		return (1);
 
 				if (*s1 != *s2)
 							return (0);
