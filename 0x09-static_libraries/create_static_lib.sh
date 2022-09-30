26850e4e3112b53f5b4e6c101fcbf9fcac268258
Halabitimothy <Halabitimoty@yahoo.com>
2022-09-30 19:42:07 +0100

    Task 0

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,9 +1,3 @@
-#!/bin/bash
-
-# a script that creates a static library
-# called liball.a from all the .c files
-# that are in the current directory
-
 gcc -c *.c
 ar -rcs liball.a *.o
 randlib liball.a
