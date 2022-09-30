c70c41b9a73003987915e103b0b6e37126b38cb3
Halabitimoty <halabitimoty@yahoo.com>
2022-09-30 13:45:15 -0700

    Task 1

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * atoi - function that converts a string
+ * _atoi - function that converts a string
  *       into an integer
  *
  * @s: string input pointer
@@ -11,4 +11,16 @@
 
 int _atoi(char *s)
 {
+	int sign = 1;
+	unsigned int num = 0;
+
+	do {
+		if (*s == '-')
+			sign *= -1;
+		else if (*s >= '0' && *s <= '9')
+			num = (num * 10) + (*s - '0');
+		else if (num > 0)
+			break;
+	} while (*s++);
+		return (num * sign);
 }
