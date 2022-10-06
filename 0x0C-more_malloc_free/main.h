432d9146569278bc7014ba0153de8bdfb0197a5b
Halabitimothy <Halabitimoty@yahoo.com>
2022-10-06 15:35:29 +0100

    Advanced Task 2

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -8,7 +8,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *malloc_checked(unsigned int b);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
-void multiply(char *s1, char *s2);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
 /*functions for 101-mul.c */
 
