178a157f515b8f0b602773c5c966a48c5c2b6f8e
Halabitimothy <Halabitimoty@yahoo.com>
2022-10-06 15:33:40 +0100

    Advanced Task 2

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -2,11 +2,62 @@
 #define MAIN_H
 
 #include <stdio.h>
+
+int _putchar(char c);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *malloc_checked(unsigned int b);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void multiply(char *s1, char *s2);
+
+/*functions for 101-mul.c */
+
+/**
+ * error_exit - prints error with _putchar
+ *              and exits with 98
+ *
+ * Return: Error 98 and exit(98)
+ */
+
+int error_exit(void)
+{
+	char *err;
+	int i;
+
+	err = "Error";
+	for (i = 0; err[i] != '\0'; i++)
+		_putchar(err[i]);
+	_putchar('\n');
+	exit(98);
+}
+
+/**
+ * check_number - checks if string has only
+ *                numbers
+ *
+ * @str: string to check
+ *
+ * Return: 0 is true 1 if false
+ */
+
+int check_number(char *str)
+{
+	while (*str != '\0')
+	{
+		if (*str < '0' || *str > '9')
+			return (1);
+		str++;
+	}
+	return (0);
+}
+
+/**
+ * _length - get the length of strings
+ *
+ * @str: string to get length of
+ *
+ * Return: length of string
+ */
 
 /*functions for 101-mul.c */
 
