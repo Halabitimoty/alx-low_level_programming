6ae015ad77254265b9f4c8abeca6754b5e45a468
Halabitimoty <57334680+Halabitimoty@users.noreply.github.com>
2022-10-12 14:06:30 +0100

    Initial Commit

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,13 +1,12 @@
 /**
- *
  * print_name - a function that prints a name.
  * @name: Character Pointer
  * @f: function pointer that doesn't return anything
  * @author Alabi Timothy
- *
  */
 
 void print_name(char *name, void (*f)(char *))
 {
-	f(name);
+	if (name && f)
+		f(name);
 }
